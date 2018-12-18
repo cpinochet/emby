680a28b87aeb7410598d9fa765739b3b533fe495
Carlos Pinochet <cpinochet@nisum.com>
2018-12-18 19:29:00 -0300

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# embyserver
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: embyserver
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
